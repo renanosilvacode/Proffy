66d26ccea48d32e41352b965183e492a2c016134
Renan Oliveira da Silva <renanosilva@gmail.com>
2020-08-05 21:46:42 -0300

    Finalizing API in NodeJS

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import ClassesControles from './controllers/ClassesController';
+import ConnectionsControllers from './controllers/ConnectionsController';
 
 const routes = express.Router();
 const classesControllers = new ClassesControles();
+const connectionsControllers = new ConnectionsControllers();
 
 
 routes.get('/classes', classesControllers.index);
 routes.post('/classes', classesControllers.create);
+
+routes.post('/connections', connectionsControllers.create);
+routes.get('/connections', connectionsControllers.index);
 
 export default routes;
